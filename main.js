86112ea1d44f1b6f2c09a599eefb3ccd5b374933
Shaurya261 <87535175+Shaurya261@users.noreply.github.com>
2022-01-13 13:48:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+canvas = document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+rrr=["mars.jpg","rover.png"];
+random_number = Math.floor(Math.random()*2);
+
+rover_width=100;rover_height=90;
+background_image = rrr[random_number];
+console.log("background_image =" + background_image);
+rover_image = "rover.png";
+rover_x = 10;rover_y = 10;
+function add()
+{
+    background_img_tag= new Image();
+    background_img_tag.onload=uploadBackground;
+    background_img_tag.src=background_image;
+
+    rover_img_tag= new Image();
+    rover_img_tag.onload=uploadRover;
+    rover_img_tag.src=rover_image;
+}
+
+function uploadBackground()
+{
+    ctx.drawImage(background_img_tag,0,0,canvas.width,canvas.height);
+
+}
+function uploadRover()
+{
+    ctx.drawImage(rover_img_tag,0,0,rover_width,rover_height);
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed == '38')
+    {
+        up();
+        console.log("up");
+    }
+    if(keyPressed == '37')
+    {
+        left();
+        console.log("left");
+    }
+    if(keyPressed == '39')
+    {
+        right();
+        console.log("right");
+    }
+    if(keyPressed == '40')
+    {
+        down();
+        console.log("down");
+    }
+}
+function up()
+{
+    if(rover_y >= 0)
+    {
+        rover_y = rover_y - 10;
+        uploadBackground();
+        uploadRover();
+    }
+}
+function down()
+{
+    if(rover_y <= 500)
+    {
+        rover_y = rover_y + 10;
+        uploadBackground();
+        uploadRover();
+    }
+}
+function left()
+{
+    if(rover_x >= 0)
+    {
+        rover_x = rover_x - 10;
+        uploadBackground();
+        uploadRover();
+    }
+}
+function right()
+{
+    if(rover_x <= 700)
+    {
+        rover_x = rover_x + 10;
+        uploadBackground();
+        uploadRover();
+    }
+}
